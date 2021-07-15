5b42b3a8981ccc21b7d28b9ef6289925f1f3c640
GYANAM-DEVELOPER <74140267+GYANAM-ui@users.noreply.github.com>
2021-07-15 19:57:51 +0530

    Add files via upload

--- views/js/home.js
+++ views/js/home.js
@@ -2,13 +2,23 @@ username = localStorage.getItem("username");
 email = localStorage.getItem("email");
 phone = localStorage.getItem("phone");
 
-document.getElementById("name").innerHTML = username;
+document.getElementById("use").innerHTML = username + "1287_fun";
+document.getElementById("user").innerHTML = "kdc";
 document.getElementById("email").innerHTML = "Email: " + email;
 document.getElementById("phone").innerHTML = "Phone no: +91 " + phone;
+
+function create() {
+    document.getElementById("gop").style.display = "none";
+    document.getElementById("gop1").style.display = "block";
+}
 
 function logout() {
     localStorage.removeItem("username");
     localStorage.removeItem("email");
     localStorage.removeItem("phone");
     window.location = "../index.html";
+}
+
+function go() {
+    window.open("https://provido-meetings-by-gyanam.herokuapp.com/");
 }
